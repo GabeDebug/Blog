5703ae8850645853da41dd1b99e88933abbcf58a
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-26 15:10:59 -0300

    Criação da ReadUser

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using Blog.Model;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
 
 namespace Blog
 {
     class Program
     {
+
+        private const string CONNECTION_STRING = @"";
         static void Main()
         {
-            
+
+        }
+
+        static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach(var user in users)
+                {
+                    Console.WriteLine($"{user.Name}");
+                }
+            };
+
         }
     }
 }
