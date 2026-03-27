35c43139cfee02e322940e25935138bc087ca3fc
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-27 14:36:29 -0300

    Criação do Metodo ReadUser

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Blog.Model;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
@@ -10,7 +11,7 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=Sua@Debug@12345;TrustServerCertificate=True";
         static void Main()
         {
-
+            
         }
 
         static void ReadUsers()
@@ -22,11 +23,18 @@ namespace Blog
 
                 foreach (var user in users)
                 {
-                    Console.WriteLine($"{user.Name}");
+                    Console.WriteLine($"{user.Bio}");
                 }
             }
-            ;
+        }
 
+         public  static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine($"{user.Name}");
+            }
         }
     }
 }
